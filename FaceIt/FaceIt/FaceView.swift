4ea34d4843f9bfa74e1adfea39588949f18414de
Li Maoqi <lmq729981607@gmail.com>
2016-09-22 11:37:17 +0800

    mouth

--- FaceIt/FaceIt/FaceView.swift
+++ FaceIt/FaceIt/FaceView.swift
@@ -12,6 +12,8 @@ class FaceView: UIView {
     
     var scale: CGFloat = 0.9
     
+    let mouthCurvature: Double = -1.0
+    
     private var skullRadius: CGFloat {
         return min(bounds.width, bounds.height) / 2 * scale
     }
@@ -49,9 +51,7 @@ class FaceView: UIView {
     private func pathForEye(eye: Eye) -> UIBezierPath {
         let eyeRadius = skullRadius / Ratios.SkullRadiusToEyeRadius
         let eyeCenter = getEyeCenter(eye)
-        let path = pathForCircleCenteredAtPoint(eyeCenter, withRadius: eyeRadius)
-        path.lineWidth = 5.0
-        return path
+        return pathForCircleCenteredAtPoint(eyeCenter, withRadius: eyeRadius)
     }
     
     private func pathForCircleCenteredAtPoint(midPoint: CGPoint, withRadius radius: CGFloat) -> UIBezierPath {
@@ -60,11 +60,33 @@ class FaceView: UIView {
         return path
     }
     
+    func pathForMouth() -> UIBezierPath {
+        let mouthWidth = skullRadius / Ratios.SkullRadiusToMouthWidth
+        let mouthHeight = skullRadius / Ratios.SkullRadiusToMouthHeight
+        let mouthOffset = skullRadius / Ratios.SkullRadiusToMouthOffset
+        
+        let mouthRect = CGRect(x: skullCenter.x - mouthWidth / 2, y: skullCenter.y + mouthOffset, width: mouthWidth, height: mouthHeight)
+        
+        let smileOffset = CGFloat( max(-1, min(mouthCurvature, 1)) ) * mouthHeight
+        
+        let startPoint = CGPoint(x: mouthRect.minX, y: mouthRect.minY)
+        let endPoint = CGPoint(x: mouthRect.maxX, y: mouthRect.minY)
+        let cp1Point = CGPoint(x: startPoint.x + mouthWidth / 3, y: startPoint.y + CGFloat(smileOffset))
+        let cp2Point = CGPoint(x: endPoint.x - mouthWidth / 3, y: endPoint.y + CGFloat(smileOffset))
+        
+        let path = UIBezierPath()
+        path.lineWidth = 5
+        path.moveToPoint(startPoint)
+        path.addCurveToPoint(endPoint, controlPoint1: cp1Point, controlPoint2: cp2Point)
+        return path
+    }
+    
     override func drawRect(rect: CGRect) {
         UIColor.blueColor().setStroke()
         pathForCircleCenteredAtPoint(skullCenter, withRadius: skullRadius).stroke()
         pathForEye(.Left).stroke()
         pathForEye(.Right).stroke()
+        pathForMouth().stroke()
     }
     
 }
